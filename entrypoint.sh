833614b45984c5bed71e79bb1b90e6b2fffc881f
Sigfrid Dusci <sigfrid@users.noreply.github.com>
2021-06-24 12:38:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ -z "${INPUT_PAT}" ]; then
+if [ -z "${INPUT_CONTAINER_PAT}" ]; then
   echo "PAT not found. Add a Personal Access Token with `read:packages` & `read:packages` permissions to the repository's secrets and then `pat: ${{ secrets.CR_PAT }}` to the workflow file."
   exit 1
 fi
@@ -9,9 +9,9 @@ fi
 RELEASE_TAG=${GITHUB_REF:10}
 
 # login with new container registry url and PAT
-echo ${INPUT_PAT} | docker login ghcr.io -u ${GITHUB_REPOSITORY_OWNER} --password-stdin
+echo ${INPUT_CONTAINER_PAT} | docker login ghcr.io -u ${GITHUB_REPOSITORY_OWNER} --password-stdin
 # new container registry urls added
-docker build --build-arg RELEASE_TAG=${RELEASE_TAG} --tag ghcr.io/${GITHUB_REPOSITORY}:${RELEASE_TAG} --cache-from ghcr.io/${INPUT_ORGANIZATION}/${REPO}:latest .
-docker push ghcr.io/${GITHUB_REPOSITORY}:${RELEASE_TAG}
+docker build --build-arg RELEASE_TAG=${RELEASE_TAG} --build-arg RR_PAT=${INPUT_RUBYGEMS_PAT} --tag ghcr.io/inforlife/${GITHUB_REPOSITORY}:${RELEASE_TAG} --cache-from ghcr.io/inforlife/${REPO}:latest .
+docker push ghcr.io/inforlife/${GITHUB_REPOSITORY}:${RELEASE_TAG}
 
 docker logout
